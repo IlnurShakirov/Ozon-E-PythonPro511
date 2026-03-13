697229a31b849ce11137e02638684a672b50c641
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-13 11:06:23 +0500

    Подключение в App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-
-import Header from '@components/UI/Header'
-
+import { useState } from 'react';
+import Header from '@components/UI/Header/Header'; 
+import Footer from '@components/Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <h1>Счётчик кликов</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Клик: {count}
-      </button>
-      <Header /> {/* Для примера */}
+      {/* Передаем title, который ждет Header */}
+      <Header title="Счётчик кликов" />
+      
+      <main style={{ textAlign: 'center', padding: '50px' }}>
+        <button onClick={() => setCount(count + 1)}>
+          Клик: {count}
+        </button>
+      </main>
+
+      {/* Передаем copyright, который ждет Footer */}
+      <Footer copyright="© 2026 Alias Team. Все права защищены." />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
