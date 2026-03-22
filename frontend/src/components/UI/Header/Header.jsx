6aa9c94d792028bfd10cb4b91a9131df6d9a2c23
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-22 12:51:38 +0500

    замена заголовка в Header-"Магазин техники"

--- frontend/src/components/UI/Header/Header.jsx
+++ frontend/src/components/UI/Header/Header.jsx
@@ -1,7 +1,7 @@
 import styles from './Header.module.css'
 
 const Header = () => {
-  return <header className={styles.header}>Шапка сайта</header>
+  return <header className={styles.header}>Магазин техники</header>
 }
 
 export default Header;
