38b20726e92b08016acc104dbc2bc91f8918134f
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-22 12:48:15 +0500

    Добавление компонента ProductList в App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
-import Header from '@components/UI/Header'
-
+import Header from '@components/UI/Header'; // путь может меняться от структуры
+import ProductList from '@components/UI/ProductList';
 
 function App() {
-  return <Header/>
+  return (
+    <div>
+      <Header />
+      
+      <ProductList />
+    </div>
+  );
 }
 
-export default App;
+
+export default App
+
+
+
+
