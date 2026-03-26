372c6a6c96abc2c90de7f5a7d495f8682d66e312
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-26 18:33:09 +0500

    Подключить компонент AddProduct в App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import Header from '@components/UI/Header'; // путь может меняться от структуры
+import AddProduct from '@components/UI/Forms/AddProduct';
+import Header from '@components/UI/Header'; 
 import ProductList from '@components/UI/ProductList';
+
+import styles from './App.module.css'
+
 
 function App() {
   return (
     <div>
       <Header />
       
-      <ProductList />
+      <main className={styles.main}>
+        <ProductList />
+      </main>  
+      <aside className={styles.aside}>
+        <AddProduct />
+      </aside>
     </div>
   );
 }
