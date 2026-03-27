1cf1afa0a9d1a705863bee2bf4b2e1347e59f264
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-27 19:41:25 +0500

    Переместить состояния items в App, для их дальнейшей передачи в Addproduct

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,53 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import AddProduct from '@components/UI/Forms/AddProduct';
 import Header from '@components/UI/Header'; 
 import ProductList from '@components/UI/ProductList';
 
 import styles from './App.module.css'
 
+const API_URL = import.meta.env.VITE_API_URL
+
 
 function App() {
+  // items = [
+  //   {id: "1", name: "Xiaomi Redmi 10", description: "Топ за свои деньги!", isAvailable: true},
+  //   {id: "2", name: "Iphone X", description: "Крутой телефон (нет) и дорогой (да)", isAvailable: false}
+  // ]
+  const [items, setItems] = useState([])
+  const [itemsLoading, setItemsLoading] = useState(true)
+  const [itemsError, setItemsError] = useState(null)
+
+ useEffect(() => {
+    axios.get(`${API_URL}/items`)
+      .then(response => {
+        setItems(response.data)
+        setItemsLoading(false)
+      })
+      .catch(error => {
+        console.error("Ошибка при получении данных:", error)
+        setItemsLoading(false)
+
+        const errMsg = 
+          error.message === "Network Error" ? "Ошибка сети" :
+          error.response?.status === 404 ? "Ресурс не найден" : 
+          "Повторите попытку позже"
+        
+        setItemsError(`Ошибка загрузки вещей: ${errMsg}`)
+      })
+  }, [])
+
   return (
     <div>
       <Header />
       
       <main className={styles.main}>
-        <ProductList />
+        <ProductList 
+          items={items} 
+          itemsLoading={itemsLoading} 
+          itemsError={itemsError}
+        />
       </main>  
       <aside className={styles.aside}>
         <AddProduct />
--- frontend/src/components/UI/ProductList/ProductList.jsx
+++ frontend/src/components/UI/ProductList/ProductList.jsx
@@ -1,36 +1,7 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { BsTrash3Fill } from 'react-icons/bs'
-import { FaEdit } from "react-icons/fa"
+import React from 'react'
 
-import styles from './ProductList.module.css'
 
-const API_URL = import.meta.env.VITE_API_URL
-
-const ProductList = () => {
-  const [items, setItems] = useState([])
-  const [itemsLoading, setItemsLoading] = useState(true)
-  const [itemsError, setItemsError] = useState(null) 
-
-  useEffect(() => {
-    axios.get(`${API_URL}/items`)
-      .then(response => {
-        setItems(response.data)
-        setItemsLoading(false)
-      })
-      .catch(error => {
-        console.error("Ошибка при получении данных:", error)
-        setItemsLoading(false)
-
-        const errMsg = 
-          error.message === "Network Error" ? "Ошибка сети" :
-          error.response?.status === 404 ? "Ресурс не найден" : 
-          "Повторите попытку позже"
-        
-        setItemsError(`Ошибка загрузки вещей: ${errMsg}`)
-      })
-  }, [])
-
+const ProductList = ({ items, itemsLoading, itemsError}) => {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Список вещей</h1>
@@ -47,20 +18,8 @@ const ProductList = () => {
             
             !itemsError && items.length > 0 ? (
               items.map(item => (
-                <li key={item.id} style={{ marginBottom: '8px' }}>
-                  <BsTrash3Fill 
-                     title="Удалить"
-                     className={styles.deleteIcon} 
-                     size={15}
-                     color="red"
-                  />
-                  <FaEdit 
-                    title="Редактировать"
-                    className={styles.editIcon} 
-                    size={15}
-                    color="black"
-                  />
-                  <strong>{item.name}</strong>: {item.description} — {item.isAvailable ? "✅ В наличии" : "❌ Нет в наличии"}
+                <li key={item.id} item={item} style={{ marginBottom: '8px' }}>
+                  
                 </li>
               ))
             ) : (
