839b71eeaf43bc621c587f009ecfd2f82a95d6a4
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-27 18:17:55 +0500

    Сохранить  данные из полей формы в состоянияю

--- frontend/src/components/UI/Forms/AddProduct/AddProduct.jsx
+++ frontend/src/components/UI/Forms/AddProduct/AddProduct.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react'
+
 import styles from './AddProduct.module.css'
 
+const AddProduct = () => {
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [isAvailable, setIsAvailable] = useState(false)
 
-const AddProduct = () => (
-    <form className={styles.form}>
-        <input type="text" placeholder='Название' />
-        <textarea placeholder='Описание'></textarea>
+    return (
+        <form className={styles.form}>
+            <input
+                type="text"
+                placeholder='Название'
+                onChange={e => setName(e.target.value)}
+            />
+            <textarea
+                placeholder='Описание'
+                onChange={e => setDescription(e.target.value)}
+            ></textarea>
 
-        <div>
-            <input type='checkbox' id='isAvailable' />
-            <label htmlFor='isAvailable'>В наличии?</label>
-        </div>
+            <div>
+                <input
+                    type='checkbox'
+                    id='isAvailable'
+                    onChange={e => setIsAvailable(e.target.checked)}
+                />
+                <label htmlFor='isAvailable'>В наличии?</label>
+            </div>
 
-        <button type='button'>Добавить</button>
-    </form>
-)
+            <button type='button' onClick={()=> {
+                console.log("name -", name)
+                console.log("description -", description)
+                console.log("isAvailable -", isAvailable)
+            }}>Добавить</button>
+        </form>
+    )
+}
 
 export default AddProduct
