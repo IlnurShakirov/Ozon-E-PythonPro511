65f4950abecd657922984602f05367ddd2c69fd1
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-03-29 23:12:25 +0500

    feat(ProductList):  Реализовать отображение фото вещей через json-server

--- frontend/src/components/UI/ProductList/ProductList.jsx
+++ frontend/src/components/UI/ProductList/ProductList.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BsTrash3Fill } from 'react-icons/bs'
+import { FaEdit } from "react-icons/fa"
+
+import styles from './ProductList.module.css'
+
+const API_URL = import.meta.env.VITE_API_URL
 
 
 const ProductList = ({ items, itemsLoading, itemsError}) => {
@@ -18,8 +24,29 @@ const ProductList = ({ items, itemsLoading, itemsError}) => {
             
             !itemsError && items.length > 0 ? (
               items.map(item => (
-                <li key={item.id} item={item} style={{ marginBottom: '8px' }}>
-                  
+                <li key={item.id} style={{ marginBottom: '8px' }}>
+                  <BsTrash3Fill 
+                     title="Удалить"
+                     className={styles.deleteIcon} 
+                     size={15}
+                     color="red"
+                  />
+                  <FaEdit 
+                    title="Редактировать"
+                    className={styles.editIcon} 
+                    size={15}
+                    color="black"
+                  /> 
+                  <strong>{item.name}</strong>
+                  <div>
+                      <img
+                          className= {styles.itemImg}
+                          src={item.imageUrl ? `${API_URL}/${item.imageUrl}` : `${API_URL}/no-image.jpg`}
+                          alt={item.imageUrl ? "Фото вещи" : 'Нет фото'}
+                      />
+                  </div>                   
+                  <div>{item.description}</div> 
+                  <div>{item.isAvailable ? " ✅  (В наличии)" : " ❌ (Нет на складе)"}</div>
                 </li>
               ))
             ) : (
