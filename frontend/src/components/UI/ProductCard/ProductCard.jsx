1112a231ea5c26592f3ddbe160f52368fd96adcc
Ilnur Shakirov <ilnur.shakirov.94@mail.ru>
2026-04-05 16:55:53 +0500

    добавил функционал удаления вещей при нажатии на соответствующую иконку

---
+++ frontend/src/components/UI/ProductCard/ProductCard.jsx
@@ -0,0 +1,35 @@
+import { BsTrash3Fill } from 'react-icons/bs'
+import { FaEdit } from "react-icons/fa"
+
+import styles from './ProductList.module.css'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+const ProductCard = ({item, onDelete}) =>
+    <li key={item.id} style={{ marginBottom: '8px' }}>
+        <BsTrash3Fill 
+            title="Удалить"
+            className={styles.deleteIcon}
+            onClick={() => onDelete(item.id)} 
+            size={15}
+            color="red"
+        />
+        <FaEdit 
+            title="Редактировать"
+            className={styles.editIcon} 
+            size={15}
+            color="black"
+        /> 
+        <strong>{item.name}</strong>
+        <div>
+            <img
+                className= {styles.itemImg}
+                src={item.imageUrl ? `${API_URL}/${item.imageUrl}` : `${API_URL}/no-image.jpg`}
+                alt={item.imageUrl ? "Фото вещи" : 'Нет фото'}
+            />
+        </div>                   
+        <div>{item.description}</div> 
+        <div>{item.isAvailable ? " ✅  (В наличии)" : " ❌ (Нет на складе)"}</div>
+    </li>
+
+export default ProductCard    
--- frontend/src/components/UI/ProductList/ProductList.jsx
+++ frontend/src/components/UI/ProductList/ProductList.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import { BsTrash3Fill } from 'react-icons/bs'
-import { FaEdit } from "react-icons/fa"
-
-import styles from './ProductList.module.css'
-
-const API_URL = import.meta.env.VITE_API_URL
+import ProductCard from `@components/UI/ProductCard`
 
 
-const ProductList = ({ items, itemsLoading, itemsError}) => {
+const ProductList = ({ items, itemsLoading, itemsError, onDelete}) => {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Список вещей</h1>
@@ -24,30 +18,7 @@ const ProductList = ({ items, itemsLoading, itemsError}) => {
             
             !itemsError && items.length > 0 ? (
               items.map(item => (
-                <li key={item.id} style={{ marginBottom: '8px' }}>
-                  <BsTrash3Fill 
-                     title="Удалить"
-                     className={styles.deleteIcon} 
-                     size={15}
-                     color="red"
-                  />
-                  <FaEdit 
-                    title="Редактировать"
-                    className={styles.editIcon} 
-                    size={15}
-                    color="black"
-                  /> 
-                  <strong>{item.name}</strong>
-                  <div>
-                      <img
-                          className= {styles.itemImg}
-                          src={item.imageUrl ? `${API_URL}/${item.imageUrl}` : `${API_URL}/no-image.jpg`}
-                          alt={item.imageUrl ? "Фото вещи" : 'Нет фото'}
-                      />
-                  </div>                   
-                  <div>{item.description}</div> 
-                  <div>{item.isAvailable ? " ✅  (В наличии)" : " ❌ (Нет на складе)"}</div>
-                </li>
+                   <ProductCard key={item.id} item={item} onDelete />         
               ))
             ) : (
               !itemsError && <div>Список пуст</div>
